532dd02ed1e21ca50b8d1b1c85243a6b5d064b7d
Rafaela Silverio <rafaelapetelin@gmail.com>
2025-01-07 17:23:17 -0300

    Cabecalho e barra lateral

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import { styled } from "styled-components"
 import EstilosGlobais from "./componentes/EstilosGlobais"
+import Cabecalho from "./componentes/Cabecalho"
+import BarraLateral from "./componentes/BarraLateral/Index"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -14,6 +16,8 @@ function App() {
   return (
     <FundoGradiente>
       <EstilosGlobais/>
+      <Cabecalho/>
+      <BarraLateral/>
     </FundoGradiente>
   )
 }
