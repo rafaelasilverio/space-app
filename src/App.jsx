332ae059cc367bd6655e4fb25a06bc12ed752ee8
Rafaela Silverio <rafaelapetelin@gmail.com>
2025-01-13 20:56:54 -0300

    finalizando aula 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-
 import { styled } from "styled-components"
 import EstilosGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
-import BarraLateral from "./componentes/BarraLateral/Index"
+import BarraLateral from "./componentes/BarraLateral"
+import Banner from "./componentes/Banner"
+import bannerBackground from './assets/banner.png'
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -15,9 +16,13 @@ function App() {
 
   return (
     <FundoGradiente>
-      <EstilosGlobais/>
-      <Cabecalho/>
-      <BarraLateral/>
+      <EstilosGlobais />
+      <Cabecalho />
+      <BarraLateral />
+      <Banner
+        texto="A galeria mais completa de fotos do espaço!"
+        backgroundImage={bannerBackground}
+      />
     </FundoGradiente>
   )
 }
--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -1,14 +1,21 @@
 import styled from "styled-components";
 
 const ItemListaEstilizado = styled.li`
-
-
+    font-size: 24px;
+    line-height: 29px;
+    margin-bottom: 30px;
+    cursor: pointer;
+    color: ${ props => props.$ativo ? '#7B78E5' : '#D9D9D9'};
+    display: flex;
+    align-items: center;
+    gap: 22px;
+    font-family: ${ props => props.$ativo ? 'GandhiSansBold' : 'GandhiSansRegular'};
 `;
 
 
 const ItemNavegacao = ({children, iconeAtivo, iconeInativo, ativo = false}) => {
   return (
-    <ItemListaEstilizado>
+    <ItemListaEstilizado $ativo={ativo}>
       <img src={ativo ? iconeAtivo : iconeInativo} alt="" />
       {children}
     </ItemListaEstilizado>
